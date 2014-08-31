641e9bda31a4aa787fabfe0344835475b5571311
Zach Banks <zbanks@mit.edu>
2014-08-31 17:45:55 -0400

    Bower

--- index.js
+++ index.js
@@ -8,6 +8,47 @@ server.listen(3000, function(){
     console.log("Server listening on port 3000...");    
 });
 
+// Load test
+var load_test = function(){
+    // return require('test.js');
+    return {
+        "title" : "Test Test",
+        "description": "Descriptions",
+        "sections": [
+            { 
+                "id": "a",
+                "number": 1,
+                "title": "Section A",
+                "description": "<em>A</em>",
+            },
+            { 
+                "id": "b",
+                "number": 2,
+                "title": "Section B",
+                "description": "<em>B</em>",
+            },
+        ],
+        "questions": [
+            {
+                "number": 1,
+                "section": "a",
+                "title": "Question 1"
+            },
+            {
+                "number": 2,
+                "section": "a",
+                "title": "Question 2"
+            },
+            {
+                "number": 3,
+                "section": "b",
+                "title": "Question 3"
+            },
+        ],
+    };
+}
+var test = load_test();
+
 // Routing
 app.use(express.static(__dirname + '/static'));
 
