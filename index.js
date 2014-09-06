fcf359086d6653f09ffcf8acbb63369a0aabab01
Zach Banks <zbanks@mit.edu>
2014-09-06 14:59:04 -0400

    Multi-user!

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var _ = require('underscore');
+var crypto = require('crypto');
+
+var sha1 = function(data){
+    var shasum = crypto.createHash('sha1');
+    shasum.update(data);
+    shasum.update("4thEAST!");
+    return shasum.digest('hex');
+}
 
 // Start the server
 server.listen(3000, function(){
@@ -60,7 +69,12 @@ app.use(express.static(__dirname + '/static'));
 // Real-time
 var clients = 1;
 io.on('connection', function(socket){
-    var clientName = clients++;
+    var clientName = (clients++).toString();
+    var clientHash = sha1(clientName);
+    socket.on("setName", function(data, callback){
+        clientName = data;
+        clientHash = sha1(clientName);
+    });
     socket.on("test:read", function(data, callback){
         callback(null, test.test);
     });
@@ -68,16 +82,23 @@ io.on('connection', function(socket){
         callback(null, test.sections);
     });
     socket.on("questions:read", function(data, callback){
-        callback(null, test.questions);
+        var questions = _.clone(test.questions);
+        _.each(questions, function(val, key){
+            questions[key].myAnswer = questions[key].answers[clientHash] || null;
+        });
+        callback(null, questions);
     });
     socket.on("question:update", function(data, callback){
-        console.log(data);
         var number = data.number;
         var answer = data.myAnswer;
-        _(test.question).findWhere({number: number}).answers[clientName] = answer;
+        var q = _(test.questions).findWhere({number: number});
+        if(q){ 
+            q.answers[clientHash] = answer;
+        }
+        data.answers[clientHash] = answer;
 
-        socket.emit("question/" + number + ":update");
-        socket.broadcast.emit("question/" + number + ":update");
-        callback(null, test.questions);
+        socket.emit("question/" + number + ":update", data);
+        socket.broadcast.emit("question/" + number + ":update", data);
+        callback(null, data);
     });
 });
